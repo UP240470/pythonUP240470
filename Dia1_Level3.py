6794582a69421e419287337ff38f6dd576079e86
UP240470 <up240470@alumnos.upa.edu.mx>
2025-06-05 11:31:06 -0600

    acabe level1 ejercicio 2

---
+++ Dia1_Level3.py
@@ -0,0 +1,13 @@
+#Level 3
+print(50, "int")          # Int
+print(5.10, "Float")        # Float
+print(5 + 4j, "complex")      # Complex number
+print('cadena', "String")  # String
+print(['a', 'b', 'c'],"list")   # List
+print({'racista':'negro'},"Dictionary") # Dictionary
+print({10.2, 5.2, 1.5},"set")    # Set
+print((11.8, 3.14, 11.15),"Tuple")    # Tuple
+
+
+x=((2-10)**2+(3-8)**2)**(1/2)
+print(x)
---
+++ Dia2.py
@@ -0,0 +1,14 @@
+Apellido=10
+Nombre="omar"
+nombre_completo=5
+Pais="mexico"
+ciudad="ags"
+edad=19
+año=2025
+casado="no"
+verdadero=1
+luz_encendida="zi"
+num_amgigos=4, lentes=10, computadoras=50
+
+
+
