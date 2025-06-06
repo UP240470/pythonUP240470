14075b1e6bae7d526e9f587499234bb45ee1c313
UP240470 <up240470@alumnos.upa.edu.mx>
2025-06-06 11:21:14 -0600

    terimar dia 2

---
+++ Dia2_level2.py
@@ -0,0 +1,107 @@
+Apellido="Facio Palos"
+Nombre="omar"
+nombre_completo=5
+Pais="mexico"
+ciudad="ags"
+edad=19
+año=2025
+casado="no"
+verdadero=1
+luz_encendida="zi"
+num_amgigos, lentes, computadoras =4,10,200
+
+
+#1.
+print(type(Apellido))
+print(type(Nombre))
+print(type(nombre_completo))
+print(type(Pais))
+print(type(ciudad))
+print(type(edad))
+print(type(año))
+print(type(casado))
+print(type(verdadero))
+print(type(luz_encendida))
+print(type(num_amgigos))
+print(type(lentes))
+print(type(computadoras ))
+
+#2.
+print(len(Nombre))
+#longitud de la palabra
+
+#3.
+print("la longitud de mi nombre  es ",len(Nombre), "mi apellido es de ", len(Apellido))
+
+#4.
+numero_uno=5
+numero_dos=4
+
+#5.
+total=numero_uno+numero_dos
+print(total)
+
+#6
+diff=numero_dos-numero_uno
+print(diff)
+
+#7.
+product=numero_dos*numero_uno
+print(product)
+
+#8.
+division=numero_uno/numero_dos
+print(division)
+
+#9.
+residuo=numero_dos%numero_uno
+print(residuo)
+
+#10.
+exp=numero_uno**numero_dos
+print(exp)
+
+#11.
+florr_division=numero_uno//numero_dos
+print(florr_division)
+
+#12.
+area_of_circle=3.14*30**2
+print("el are es de", area_of_circle)
+
+#12.2
+circuferencia=3.1416*30*2
+print(circuferencia)
+
+#12.3
+valor_usuario=float (input ("dame el radio"))
+print("el area es de: ", 3.1416*valor_usuario**2)
+
+#13.
+nombre_usuario=str(input("Dime tu nombre"))
+apellido_usuario=str(input("Dime tu apellido"))
+pais_usuario=str(input("Dime tu pais"))
+edad_usuario=str(input("Dame tu edad"))
+
+print("tu nombre es ",nombre_usuario,"nu apellido es ",apellido_usuario,"Tu pais es ",pais_usuario,"Tu edad es ",edad)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
