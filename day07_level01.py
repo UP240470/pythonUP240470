6e9f3bf2a9c3fdd6678edfccf7560f298b99a4a9
UP240470 <up240470@alumnos.upa.edu.mx>
2025-07-16 18:13:00 -0600

    Termine dia 7 level1

---
+++ day07_level01.py
@@ -0,0 +1,37 @@
+#Sets
+it_companies={'Facebook',"Google","Microsoft",'apple','IBM','oracle','Amazon'}
+A= {19,22,24,20,25,26}
+B= {19,22,20,25,26,24,28,27}
+age=[22,19,24,25,26,24,25,24]
+
+#1
+print("Longitud de it_companies:",len(it_companies))
+
+#2
+it_companies.add('Twitter')
+print('Agregar Twitter:',it_companies)
+
+#3
+it_companies.update(['Tesla','Samsung','Intel'])
+print("Agregar multiples compañias:", it_companies)
+
+#4
+it_companies.remove('Samsung')
+print("Despues de eliminar Samsung:",it_companies)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
